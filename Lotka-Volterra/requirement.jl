74e42e990e50bde5682f22b0cd6e6eff9c726913
vyuma <bohda.yuuma.f5@s.mail.nagoya-u.ac.jp>
2024-10-23 14:22:24 +0000

    docker環境の整備とjulia環境の整備

---
+++ Lotka-Volterra/requirement.jl
@@ -0,0 +1,24 @@
+# Juliaのパッケージ管理を使用
+using Pkg
+
+# 必要なパッケージをインストール
+Pkg.add("Random")
+Pkg.add("Lux")
+Pkg.add("LinearAlgebra")
+Pkg.add("NNlib")
+Pkg.add("ConcreteStructs")
+Pkg.add("WeightInitializers")
+Pkg.add("ChainRulesCore")
+Pkg.add("ComponentArrays")
+Pkg.add("BenchmarkTools")
+Pkg.add("OrdinaryDiffEq")
+Pkg.add("Plots")
+Pkg.add("DiffEqFlux")
+Pkg.add("ForwardDiff")
+Pkg.add("Flux")
+Pkg.add("Optimisers")
+Pkg.add("MAT")
+Pkg.add("ProgressBars")
+Pkg.add("Zygote")
+Pkg.add("SymbolicRegression")
+Pkg.add("SymbolicUtils")
